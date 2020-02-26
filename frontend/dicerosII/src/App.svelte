5c54d88f7c231f3338b61b0f61cdec6ba64151c1
capulusnoctis <axel.solares.g@gmail.com>
2020-02-26 09:23:09 -0600

    Added style to login screen

--- frontend/dicerosII/src/App.svelte
+++ frontend/dicerosII/src/App.svelte
@@ -5,7 +5,7 @@
 <main>
 
 	<figure>
-		<img src="#" alt="diceros">
+		<img src="diceros.jpg" alt="diceros">
 		<figcaption>{name}</figcaption>
 	</figure>
 	
@@ -26,27 +26,38 @@
 			<input type="submit" value="Iniciar Sesion">
 		</div>
 	</form>
+
 </main>
+
+<div class="cosmic1"></div>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		padding: 1rem;
+		max-width: 600px;
 		margin: 0 auto;
 		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 	}
 
 	figure {
-		margin-top: 100px;
-		margin-bottom: 40px;
+		margin-top: 1rem;
+		margin-bottom: 2rem;
+		width: auto;
+		height: auto;
 	}
 
 	figure figcaption {
 		color: #013467;
 		text-transform: uppercase;
-		font-size: 3em;
+		font-size: 2.5em;
 		font-weight: 100;
+		margin-top: -1rem;
+	}
+
+	figure img {
+		width: auto;
+		height: 150px;
 	}
 
 	form {
@@ -54,14 +65,20 @@
 		width: 350px;
 		height: 250px;
 		border: 1px solid black;
-		padding: 25px 15px;
+		padding: 30px 10px 45px 10px;
 		border-radius: 8px;
 		font-size: 14px;
+		background-color: #f5faff;
 	}
 
 	form div {
 		text-align: center;
-		padding: 5px 10px;
+		padding: 8px 4px;
+	}
+
+	form div:first-child{
+		padding: 2px 4px 16px 4px;
+		margin-top: 0;
 	}
 
 	form div span#login {
@@ -84,10 +101,11 @@
 		border-left: 1px transparent;
 		border-right: 1px transparent;
 		border-bottom: 1px solid #013467;
-		margin: auto;
+		margin: 0 auto;
 		padding: 10px;
 		outline: none;
 		width: 60%;
+		background-color: inherit;
 	}
 
 	form div input:not([type='submit'])::placeholder {
@@ -97,16 +115,28 @@
 	form div input[type='submit'] {
 		padding: 10px 15px;
 		margin-top: 20px;
-		background-color: #4188ce;
+		background-color: #b3d9ff;
 		color: #013467;
 		font-weight: 600;
 		border: 0px solid transparent;
 		cursor: pointer;
+		outline: none;
+	}
+
+	.cosmic1 {
+		width: 500px;
+		z-index: -2;
+		height: 800px;
+		background-color: #4188ceb7;
+		transform: rotate(78deg);
+		position: absolute;
+		top: 385px;
+		left: 385px;
 	}
 
 	@media (min-width: 640px) {
 		main {
-			max-width: none;
+			max-width: 100%;
 		}
 	}
 </style>
